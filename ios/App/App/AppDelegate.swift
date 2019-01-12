9348b53326c5196a5c6574e77aff24664872c2e1
Drew Frisk <hello@drewfrisk.com>
2019-01-12 11:07:14 -0600

    disable screen sleep on iOS & android

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    // keep th the device awake while the app is running
+    UIApplication.shared.isIdleTimerDisabled = true
+    UIApplication.shared.setStatusBarHidden(true, with: UIStatusBarAnimation.none)
+    
     // Override point for customization after application launch.
     return true
   }

d92e4f5336ea222f5e32b9625547700c65cb477b
Drew Frisk <hello@drewfrisk.com>
2019-01-16 20:59:31 -0600

    need to grant permissions to ruby script

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -14,3 +14,7 @@ sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 ##
 (cd ../../ && npm install)
 (cd ../../ && npm run build)
+
+
+## grant permission to run ruby script
+chmod +x ./create-appcenter-credentials.rb

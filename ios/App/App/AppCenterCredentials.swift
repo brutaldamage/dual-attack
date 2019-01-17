e673164f757650cd3c6feb6304cca317ae5a4772
Drew Frisk <hello@drewfrisk.com>
2019-01-16 20:33:47 -0600

    trying a different script process for ios appcenter secrets

---
+++ ios/App/App/AppCenterCredentials.swift
@@ -0,0 +1,13 @@
+//
+//  AppCenterCredentials.swift
+//  App
+//
+//  Created by Drew on 1/16/19.
+//
+
+import Foundation
+
+
+struct AppCenterCredentials {
+    static let appSecret = ""
+}
---
+++ ios/App/create-appcenter-credentails.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+file_content = <<-CREDS_FILE_STRING
+struct AppCenterCredentials {
+    static let appSecret = "#{ENV['APP_CENTER_SECRET']}"
+}
+CREDS_FILE_STRING
+
+file = File.new("App/AppCenterCredentials.swift", "w")
+file.puts(file_content)
+file.close
